dca5ed22666a18462adc694a817b71adc76bc1bf
tduck973564 <tduck973564@gmail.com>
2021-08-28 18:38:02 +1000

    fixed some issues stopping this from working

--- build.sh
+++ build.sh
@@ -3,3 +3,4 @@ echo Install qemu-system-x86 before proceeding.
 rustup override set nightly
 rustup component add llvm-tools-preview
 cargo install bootimage
+rustup component add rust-src --toolchain nightly-x86_64-unknown-linux-gnu
